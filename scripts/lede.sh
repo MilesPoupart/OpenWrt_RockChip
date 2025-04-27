167623b5ce0dcd01f4917d5b6e8cf920415d212a
MilesPoupart <renjunyang007@126.com>
2025-04-27 15:00:44 +0800

    taskplan: new package name

--- scripts/lede.sh
+++ scripts/lede.sh
@@ -102,8 +102,8 @@ git clone --depth=1 https://github.com/sirpdboy/NetSpeedTest
 
 # Add luci-app-autotimeset
 rm -rf ../../customfeeds/luci/applications/luci-app-autotimeset
-git clone --depth=1 https://github.com/sirpdboy/luci-app-autotimeset
-sed -i "s/\"control\"/\"system\"/g" luci-app-autotimeset/luasrc/controller/autotimeset.lua
+git clone --depth=1 https://github.com/sirpdboy/luci-app-taskplan
+sed -i "s/\"control\"/\"system\"/g" luci-app-taskplan/luasrc/controller/taskplan.lua
 
 # Add mosdns
 rm -rf ../../customfeeds/packages/net/mosdns
