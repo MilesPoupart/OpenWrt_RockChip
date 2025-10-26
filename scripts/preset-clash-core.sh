4f6a95a42d71bf74646776cc23ce09ef1d804dd4
MilesPoupart <renjunyang007@126.com>
2025-10-26 23:41:51 +0800

    scripts: diable smart clash core

--- scripts/preset-clash-core.sh
+++ scripts/preset-clash-core.sh
@@ -14,8 +14,8 @@ CLASH_SMART_URL="https://raw.githubusercontent.com/vernesong/OpenClash/core/mast
 GEOIP_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat"
 GEOSITE_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat"
 
-# wget -qO- $CLASH_META_URL | tar xOvz > files/etc/openclash/core/clash_meta
-wget -qO- $CLASH_SMART_URL | tar xOvz > files/etc/openclash/core/clash_meta
+wget -qO- $CLASH_META_URL | tar xOvz > files/etc/openclash/core/clash_meta
+# wget -qO- $CLASH_SMART_URL | tar xOvz > files/etc/openclash/core/clash_meta
 wget -qO- $GEOIP_URL > files/etc/openclash/GeoIP.dat
 wget -qO- $GEOSITE_URL > files/etc/openclash/GeoSite.dat
 
