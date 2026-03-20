56fff46c7defc5fb20f406edeb830110a6d25512
MilesPoupart <renjunyang007@126.com>
2026-03-21 00:22:39 +0800

    use default rust and latest golang

--- scripts/immortal.sh
+++ scripts/immortal.sh
@@ -48,12 +48,7 @@ function github_partial_clone() {
 
 # Gloang
 rm -rf feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 25.x feeds/packages/lang/golang
-
-# Rust
-rm -rf feeds/packages/lang/rust
-# use https://github.com/coolsnowwolf/packages/tree/master/lang/rust
-github_partial_clone coolsnowwolf packages use_default_branch lang/rust feeds/packages/lang/rust
+git clone https://github.com/sbwml/packages_lang_golang feeds/packages/lang/golang
 
 # Docker ecosystem
 rm -rf feeds/packages/utils/docker
