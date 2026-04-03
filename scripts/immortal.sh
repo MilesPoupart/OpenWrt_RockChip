a959fea0c3c9e892cace76802d8018051b71fbf3
MilesPoupart <renjunyang007@126.com>
2026-04-03 23:27:55 +0800

    fix(immortal): switch zerotier source to sbwml's feed
    Replace the github_partial_clone from immortalwrt with a direct shallow clone from sbwml's dedicated zerotier feed repository. The previous partial clone method is commented out for potential rollback.

--- scripts/immortal.sh
+++ scripts/immortal.sh
@@ -146,7 +146,8 @@ git clone --depth=1 https://github.com/pymumu/openwrt-smartdns ../../customfeeds
 
 # Add zerotier
 rm -rf ../../customfeeds/packages/net/zerotier
-github_partial_clone immortalwrt packages master net/zerotier ../../customfeeds/packages/net/zerotier
+git clone --depth=1 https://github.com/sbwml/feeds_packages_net_zerotier.git ../../customfeeds/packages/net/zerotier
+# github_partial_clone immortalwrt packages master net/zerotier ../../customfeeds/packages/net/zerotier
 
 # Add luci-app-ustreamer
 rm -rf ../../customfeeds/luci/applications/luci-app-ustreamer
