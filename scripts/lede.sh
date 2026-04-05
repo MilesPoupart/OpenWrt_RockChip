3c6a9ec79f2bdd5e2267f232bcf26d054d59ffb2
MilesPoupart <renjunyang007@126.com>
2026-04-05 20:51:41 +0800

    refactor(lede): move luci-app-dockerman to community packages with 24.10 branch
    Move luci-app-dockerman from feeds/luci/applications to package/community
    and switch to openwrt-24.10 branch for better version compatibility.

--- scripts/lede.sh
+++ scripts/lede.sh
@@ -88,10 +88,11 @@ git clone --depth=1 https://github.com/sbwml/packages_utils_docker.git feeds/pac
 git clone --depth=1 https://github.com/sbwml/packages_utils_dockerd.git feeds/packages/utils/dockerd
 git clone --depth=1 https://github.com/sbwml/packages_utils_runc.git feeds/packages/utils/runc
 git clone --depth=1 https://github.com/sbwml/packages_utils_containerd.git feeds/packages/utils/containerd
-git clone --depth=1 https://github.com/sbwml/luci-app-dockerman.git feeds/luci/applications/luci-app-dockerman
 
 mkdir -p package/community
 pushd package/community
+
+git clone --depth=1 -b openwrt-24.10 https://github.com/sbwml/luci-app-dockerman.git
 
 # Add luci-app-watchcat-plus
 rm -rf ../../customfeeds/luci/applications/luci-app-watchcat-plus
