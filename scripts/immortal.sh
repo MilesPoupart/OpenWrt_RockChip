768515afb74ae9af88ac68917f06d0322ce67bb4
MilesPoupart <renjunyang007@126.com>
2026-05-12 22:27:59 +0800

    feat(scripts): 添加对 Rust 语言包和 luci-app-zerotier 的支持
    - 在脚本中增加了 Rust 语言包的克隆操作
    - 替换了 zerotier 软件包的仓库地址和克隆方式
    - 新增 luci-app-zerotier 的部分克隆功能调用
    - 清理并移除旧的 zerotier luci 应用目录

--- scripts/immortal.sh
+++ scripts/immortal.sh
@@ -61,6 +61,10 @@ function github_partial_clone() {
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang feeds/packages/lang/golang
 
+# Rust
+rm -rf feeds/packages/lang/rust
+git clone --depth=1 https://github.com/sbwml/packages_lang_rust feeds/packages/lang/rust
+
 # Docker ecosystem
 rm -rf feeds/packages/utils/docker
 rm -rf feeds/packages/utils/dockerd
@@ -139,8 +143,10 @@ git clone --depth=1 https://github.com/MilesPoupart/openwrt-smartdns ../../custo
 
 # Add zerotier
 rm -rf ../../customfeeds/packages/net/zerotier
-git clone --depth=1 https://github.com/MilesPoupart/feeds_packages_net_zerotier.git ../../customfeeds/packages/net/zerotier
-# git clone --depth=1 https://github.com/sbwml/feeds_packages_net_zerotier.git ../../customfeeds/packages/net/zerotier
+rm -rf ../../customfeeds/luci/applications/luci-app-zerotier
+git clone --depth=1 https://github.com/sbwml/feeds_packages_net_zerotier.git ../../customfeeds/packages/net/zerotier
+github_partial_clone sbwml openwrt_pkgs use_default_branch luci-app-zerotier luci-app-zerotier
+# git clone --depth=1 https://github.com/MilesPoupart/feeds_packages_net_zerotier.git ../../customfeeds/packages/net/zerotier
 # github_partial_clone immortalwrt packages master net/zerotier ../../customfeeds/packages/net/zerotier
 
 # Add luci-app-ustreamer
