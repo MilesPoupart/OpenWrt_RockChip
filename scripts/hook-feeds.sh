a936c07e79517b3d3232a327a16de5b66307d0f4
MilesPoupart <renjunyang007@126.com>
2026-07-12 13:47:29 +0800

    refactor(scripts): 清理和调整feed及仓库克隆配置
    - 删除了多个废弃的Lede-rockchip源选项
    - 清理重复的zerotier源修改
    - 恢复qbittorrent使用sbwml源

--- scripts/hook-feeds.sh
+++ scripts/hook-feeds.sh
@@ -75,10 +75,6 @@ clone_and_cleanup "packages/utils/watchcat" "openwrt" "packages" "use_default_br
 # Replace adguardhome
 clone_and_cleanup "packages/net/adguardhome" "immortalwrt" "packages" "use_default_branch" "net/adguardhome" "packages/net/adguardhome"
 
-# Replace luci-app-zerotier
-clone_and_cleanup "luci/applications/luci-app-zerotier" "immortalwrt" "luci" "use_default_branch" "applications/luci-app-zerotier" "luci/applications/luci-app-zerotier"
-clone_and_cleanup "packages/net/zerotier" "MilesPoupart" "feeds_packages_net_zerotier" "main" "." "packages/net/zerotier"
-
 # add missing packages
 clone_and_cleanup "luci/applications/luci-app-wireguard" "MilesPoupart" "luci" "master" "applications/luci-app-wireguard" "luci/applications/luci-app-wireguard"
 clone_and_cleanup "luci/applications/luci-app-adbyby-plus" "MilesPoupart" "luci" "master" "applications/luci-app-adbyby-plus" "luci/applications/luci-app-adbyby-plus"
